6f9c337ed94ca54336786de2fa1fc496f563f4f7
Zeptar <99463015+Zeptar1069@users.noreply.github.com>
2022-06-24 16:18:43 +1200

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-let count = localStorage.getItem("page_view");
-count = 1;
-localStorage.setItem("page_view", 1);
-count = Number(visitCount) + 1;
-localStorage.setItem("page_view", visitCount);
-
-console.log(count)
