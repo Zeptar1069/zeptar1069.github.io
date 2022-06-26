56ec3eaf7fe6012509e14c7c0bc33f5a45d8f9c7
Zeptar <99463015+Zeptar1069@users.noreply.github.com>
2022-06-26 14:21:01 +1200

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
 function websiteVisits(response) {
-  const label = document.getElementById('containerViews');
-  console.log(response.value);
+  document.getElementById('containerViews').querySelector("p").innerHTML = response.value;
 };
